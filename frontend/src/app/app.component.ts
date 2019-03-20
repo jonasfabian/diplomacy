0fa030ad8c4f43ab5586fcc0a8cd531117cb044a
Jonas Fabian <jonas.fabian@fhnw.ch>
2019-03-20 15:29:51 +0100

    wip

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import {CountryService} from './services/country.service';
+import {CountryService, CurrentCountryViewEnum} from './services/country.service';
+import {Country} from './models/country';
+import {RelationService} from './services/relation.service';
+import {CombatService} from './services/combat.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +10,23 @@ import {CountryService} from './services/country.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+
   constructor(
-    public countryService: CountryService
+    public countryService: CountryService,
+    private relationService: RelationService,
+    private combatService: CombatService,
   ) {
+  }
+
+  viewCountryEnum: any = CurrentCountryViewEnum;
+
+  showCountryDetails(country: Country): void {
+    this.relationService.displayCountryRelations(country);
+    this.countryService.country = country;
+    this.relationService.getRelationsNamed(this.countryService.country.id);
+    this.countryService.calculateNumberOfCurrencyUsers();
+    this.countryService.getManpower(country.id);
+    this.combatService.getModifiersForCountry();
+    this.countryService.currentView = this.viewCountryEnum.COUNTRYDETAIL;
   }
 }
